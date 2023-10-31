b7a8b4840c3faa117e3d670810c6b272fd7e6c66
sakil-ahmed <sakilahmed.dev@gmail.com>
2023-11-01 02:04:36 +0600

    implement topic and note feature

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,13 +1,11 @@
-import { postRouter } from "@/server/api/routers/post";
-import { createTRPCRouter } from "@/server/api/trpc";
+import {createTRPCRouter} from "@/server/api/trpc";
+import {topicRouter} from "@/server/api/routers/topic";
+import {noteRouter} from "@/server/api/routers/note";
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
+
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+    topic: topicRouter,
+    note: noteRouter,
 });
 
 // export type definition of API
